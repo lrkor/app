b82326bcc5239003925263d9ae5b6e17684ddd63
lrkor <1055409319@qq.com>
2018-12-27 18:28:09 +0800

    上拉加载

--- app/pages/engineering/engineering.js
+++ app/pages/engineering/engineering.js
@@ -3,7 +3,8 @@ var util = require('../../utils/util.js') //引入微信自带的日期格式化
 const app = getApp();
 Page({
   data: {
-    motto: 'Hello World',
+    id: '1',
+    page: 1,
     userInfo: {},
     hasUserInfo: false,
     navData: [
@@ -19,19 +20,24 @@ Page({
   //事件处理函数
   onLoad: function () {
     this.queryHeaderList();
-    this.getRecommend();
+    this.getRecommend('0','0');
 
   },
   switchNav(event) {
     var cur = event.currentTarget.dataset.current;
-
+    let page1 = 1;
+ 
     // 获取导航栏id
     var id = event.currentTarget.dataset.id;
+    this.setData({
+      id: id,
+      page:page1
+    })
 
     if (id == '1') {
-
-    }else{
-      this.getRecommend(id);
+      this.getRecommend('0','0');
+    } else {
+      this.getOtherlisr(id, '0','0');
     }
 
 
@@ -68,15 +74,35 @@ Page({
     })
   },
 
+  //下拉刷新
   onPullDownRefresh() {
     wx.showNavigationBarLoading() //在标题栏中显示加载
-    // wx.hideNavigationBarLoading() //在标题栏中显示加载
+    let id = this.data.id;
+    let page1 = 1;
+    this.setData({
+      page:page1
+    });
+    if (this.data.id == '1') {
+      this.getRecommend('1','1');
+    } else {
+      this.getOtherlisr(id, '1','1');
+    }
+  },
 
-    // wx.startPullDownRefresh({
-    //   success:function(){
-    //     wx.stopPullDownRefresh();                       //停止下拉刷新
-    //   }
-    // })
+  //上拉加载
+  onReachBottom() {
+    let id = this.data.id;
+    // 显示加载图标
+    wx.showLoading();
+    let page1 = this.data.page + 1;
+    this.setData({
+      page:page1
+    });
+    if (this.data.id == '1') {
+      this.getRecommend('1','1');
+    } else {
+      this.getOtherlisr(id, '1','1');
+    }
   },
 
   // 获取导航栏列表
@@ -101,17 +127,26 @@ Page({
   },
 
   // 获取推荐列表
-  getRecommend() {
+  getRecommend(isRefresh, isLoading) {
     var that = this;
     wx.request({
       url: 'http://192.168.1.40:8081/applet/api/article/query',
       method: 'POST',
       dataType: 'json',
-      data: {isRecommend:'1',page:'1',size:10},
+      data: { isRecommend: '1', page: that.data.page, size: 10, isRelease: '1' },
       header: {
         'content-type': 'application/json'
       },
       success: function (res) {
+        if (isRefresh == '1') {
+          wx.hideNavigationBarLoading() //在标题栏中隐藏加载
+          wx.stopPullDownRefresh();                       //停止下拉刷新
+        }
+        if (isLoading == '1') {
+          // 隐藏加载框
+          wx.hideLoading();
+        }
+
         let RecommendArr = res.data.data;
 
         // 格式化时间
@@ -126,16 +161,26 @@ Page({
   },
 
   // 获取其他列表
-  getOtherlisr(id) {
+  getOtherlisr(id, isRefresh, isLoading) {
     var that = this;
     wx.request({
       url: 'http://192.168.1.40:8081/applet/api/article/query',
-      data: { id: id, page: '1', size: '10' },
+      data: { categoryId: id, page: that.data.page, size: '10', isRelease: '1' },
       method: 'POST',
       header: {
         'content-type': 'application/json'
       },
       success: function (res) {
+        if (isRefresh == '1') {
+          wx.hideNavigationBarLoading() //在标题栏中隐藏加载
+          wx.stopPullDownRefresh();                       //停止下拉刷新
+        }
+
+        if (isLoading == '1') {
+          // 隐藏加载框
+          wx.hideLoading();
+        }
+
         let RecommendArr = res.data.data;
 
         // 格式化时间
