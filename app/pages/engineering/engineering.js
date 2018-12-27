f2fdf4b63074080997d21f8ff57b0641972e51ce
lrkor <1055409319@qq.com>
2018-12-27 11:48:15 +0800

    工程圈接口更新

--- app/pages/engineering/engineering.js
+++ app/pages/engineering/engineering.js
@@ -1,34 +1,13 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+//engineering.js
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     navData: [
       {
-        text: '推荐'
-      },
-      {
-        text: '时事资讯'
-      },
-      {
-        text: '安全资料'
-      },
-      {
-        text: '法规标准'
-      },
-      {
-        text: '案列分析'
-      },
-      {
-        text: '纠纷'
-      },
-      {
-        text: '青葱'
+        id:'1',
+        name: '推荐'
       }
     ],
     currentTab: 0,
@@ -70,52 +49,29 @@ Page({
   },
   //事件处理函数
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-
-
-    wx.getSystemInfo({
-      success: (res) => {
-        this.setData({
-          pixelRatio: res.pixelRatio,
-          windowHeight: res.windowHeight,
-          windowWidth: res.windowWidth
-        })
-      },
-    })
+    this.queryHeaderList();
+    this.getRecommend();
+   
   },
   switchNav(event) {
     var cur = event.currentTarget.dataset.current;
+
+    // 获取导航栏id
+    var id = event.currentTarget.dataset.id;
+
+    if(id=='1'){
+
+    }
+
+
     //每个tab选项宽度占1/5
     var singleNavWidth = this.data.windowWidth / 5;
+
     //tab选项居中                            
     this.setData({
       navScrollLeft: (cur - 2) * singleNavWidth
     })
+
     if (this.data.currentTab == cur) {
       return false;
     } else {
@@ -151,4 +107,44 @@ Page({
     //   }
     // })
   },
+
+  // 获取导航栏列表
+  queryHeaderList(){
+    var that = this;
+    wx.request({  
+      url: 'http://192.168.1.40:8081/applet/api/articleCategory/query',  
+      data:{},  
+      method:'POST',  
+      header: {  
+        'content-type': 'application/json'  
+      },  
+      success: function (res) {  
+        let headerArr =  res.data.data;
+        let newNavData = that.data.navData.concat(headerArr);
+        that.setData({
+          navData:newNavData
+        });
+      }  
+    }) 
+  },
+
+  // 获取推荐列表
+  getRecommend(){
+    var that = this;
+    wx.request({  
+      url: 'http://192.168.1.40:8081/applet/api/article/query',  
+      data:{isRecommend:'1'},  
+      method:'POST',  
+      header: {  
+        'content-type': 'application/json'  
+      },  
+      success: function (res) {  
+        // let headerArr =  res.data.data;
+        // let newNavData = that.data.navData.concat(headerArr);
+        // that.setData({
+        //   navData:newNavData
+        // });
+      }  
+    }) 
+  }
 })
