e11feaca4b267de465b5770d685a01219b579da9
viki <1348510416@qq.com>
2018-12-27 16:07:50 +0800

    小程序

--- app/pages/personal/personal.js
+++ app/pages/personal/personal.js
@@ -3,9 +3,18 @@
 const app = getApp()
 
 Page({
-    data:{
-        name:'张三',
-        engineeringName:'五峰山接线工程',
-        orgName:'南通一标'
-    }
+  data: {
+    name: '张三',
+    engineeringName: '五峰山接线工程',
+    orgName: '南通一标',
+  },
+  onLoad: function (options) {
+    var that = this;
+    that.setDate({
+      mername: options.mername
+    })
+    wx.setNavigationBarTitle({
+      title: that.data.mername,
+    })
+  }
 })
