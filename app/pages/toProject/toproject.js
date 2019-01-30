b7f106ac17221e8d9c0be2a3074f166a22dbaded
徐文斌 <1055409319@qq.com>
2019-01-30 17:17:52 +0800

    99

--- app/pages/toProject/toproject.js
+++ app/pages/toProject/toproject.js
@@ -7,7 +7,6 @@ Page({
   onLoad: function (options) {
     let url = options.url;
     let index = url.indexOf('loginTo');
-    console.log(index);
     let url1 = url.substring(0,index+7);
     let str = url.substring(index+8,url.length);
     let url2 = url1 + '?userName=' +str;
