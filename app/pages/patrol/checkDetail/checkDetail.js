facd7c9677fbb2365f37ce357ddf8d78d98be348
徐文斌 <1055409319@qq.com>
2019-03-13 14:50:10 +0800

    检查编辑页面

--- app/pages/patrol/checkDetail/checkDetail.js
+++ app/pages/patrol/checkDetail/checkDetail.js
@@ -1,12 +1,26 @@
 const app = getApp()
 Page({
   data: {
-  
+    state:3,
+    result:2
   },
   onLoad: function (options) {
   
   },
 
+  // 编辑
+  
 
+  // 分享
+  onShareAppMessage(res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '检查详情',
+      path: '/pages/patrol/checkDetail/checkDetail'
+    }
+  }
 
 })
