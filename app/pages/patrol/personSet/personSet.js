648728a96ae230e5c665b43b7d6453ef98b32a7b
徐文斌 <1055409319@qq.com>
2019-04-04 17:55:18 +0800

    11

--- app/pages/patrol/personSet/personSet.js
+++ app/pages/patrol/personSet/personSet.js
@@ -1,9 +1,16 @@
 const app = getApp();
 const wxRequest = require('../../../utils/wxRequest.js');
+import Dialog from '../../../miniprogram_npm/vant-weapp/dialog/dialog';
 Page({
   data: {
     type: '',
-    unitList: []
+    list: [
+      {name:'11',id:1},
+      {name:'22',id:2},
+      {name:'33',id:3},
+      {name:'44',id:4},
+      {name:'55',id:5},
+    ]
   },
   onLoad: function (options) {
     let type = options.type;
@@ -20,5 +27,20 @@ Page({
     wx.navigateTo({
       url: '../choosePerson/choosePerson?type=' + type,
     })
+  },
+  onClose(event) {
+    console.log(event);
+    const {id} = event.currentTarget.dataset
+    const { position, instance } = event.detail;
+    switch (position) {
+      case 'right':
+        Dialog.confirm({
+          message: '确定删除吗？'
+        }).then(() => {
+          console.log(id);
+          instance.close();
+        });
+        break;
+    }
   }
 })
